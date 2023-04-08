9d62674493cb8660f76c991ef9b115456f12cbd1
jehad-mansra <jehad.mansra@gmail.com>
2023-04-08 23:26:21 +0300

    first commit

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+var x = document.getElementsByClassName("g-bar");
+
+x[0].addEventListener("click", jfyn);
+function jfyn() {
+    if(y[0].style.display == "none"){
+        y[0].style.display = "flex";
+    }
+    else{
+        y[0].style.display = "none"
+    }
+}
+var y = document.getElementsByClassName("burger");
